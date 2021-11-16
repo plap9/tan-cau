fcb9c19d0b263179dbb687f7a9e92030504216e0
plap9 <86374899+plap9@users.noreply.github.com>
2021-11-17 00:12:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Manh à!',
+    introDesc: `Bạn thì cũng không thích dài dòng nên là bạn thích Manhh`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Manh có thích bạn khum 👉👈',
+    desc: 'Nếu có thì mình đến với nhau nhé <3 ',
+    btnYes: 'Vẫn cứ là thích bạn <33',
+    btnNo: 'Không, bạn à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
